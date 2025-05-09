6f68e9fe0fb7b9dfc97e7afac3c40b60c18bad62
aishvarya779 <aishvaryatiwari123@gmail.com>
2025-05-09 15:28:41 +0530

    themeing and color variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@/components/ui/button';
+import { CirclePlus } from 'lucide-react';
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="h-screen flex items-center justify-center">
-      <Button variant="ghost" className="absolute top-4 left-4">
+      <Button variant="destructive" className="rounded-full text-dash-primary" size="lg">
+        <CirclePlus />
         Click me
       </Button>
     </div>
