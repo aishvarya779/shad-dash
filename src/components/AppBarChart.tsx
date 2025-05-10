67d3adad1c3bb60cdfd15471953ff995b20ad372
aishvarya779 <aishvaryatiwari123@gmail.com>
2025-05-10 10:45:52 +0530

    charts

--- src/components/AppBarChart.tsx
+++ src/components/AppBarChart.tsx
@@ -1,15 +1,15 @@
 "use client";
 import { ChartContainer, ChartConfig } from "@/components/ui/chart"
-import { Bar, BarChart, CartesianGrid } from 'recharts';
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from 'recharts';
 
 const chartConfig = {
     desktop: {
         label: "Desktop",
-        color: "#2563eb",
+        color: "var(--chart-1)",
     },
     mobile: {
         label: "Mobile",
-        color: "#60a5fa",
+        color: "var(--chart-2)",
     },
 } satisfies ChartConfig
 
@@ -26,11 +26,23 @@ const chartData = [
 const AppBarChart = () => {
     return (
         <div className=''>
-            <h1>Total Revenue</h1>
+            <h1 className='text-lg font-medium mb-6'>Total Revenue</h1>
             <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
 
                 <BarChart accessibilityLayer data={chartData}>
                     <CartesianGrid vertical={false} />
+                    <XAxis
+                        dataKey="month"
+                        tickLine={false}
+                        tickMargin={10}
+                        axisLine={false}
+                        tickFormatter={(value) => value.slice(0, 3)}
+                    />
+                    <YAxis
+                        tickLine={false}
+                        tickMargin={10}
+                        axisLine={false}
+                    />
                     <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
                     <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
                 </BarChart>
