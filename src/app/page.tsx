fe6c5b22f476049dd349328451d8ff75704b85f2
aishvarya779 <aishvaryatiwari123@gmail.com>
2025-05-10 11:13:45 +0530

    pie chart added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 import AppAreaChart from '@/components/AppAreaChart';
 import AppBarChart from '@/components/AppBarChart';
-import { Button } from '@/components/ui/button';
-import { CirclePlus } from 'lucide-react';
-import Image from "next/image";
+import AppPieChart from '@/components/AppPieChart';
 
 export default function Home() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2"><AppBarChart /></div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
-      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg"><AppPieChart /></div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2"><AppAreaChart />  </div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
